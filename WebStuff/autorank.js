ec31805cc6107cb88bf5b43d353f624ed5b57fcc
caioband <65964765+caioband@users.noreply.github.com>
2022-08-12 03:21:25 -0300

    Update

--- WebStuff/autorank.js
+++ WebStuff/autorank.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json())
+
 const PORT = process.env.PORT || 2944;
 
 app.get('/',(req,res) => {
    return res.json({msg: 'ok'})
 })
 
-app.post("/", (req, res) => {
+app.post("/seila", (req, res) => {
     console.log(req.body);
     return req.body
 })
